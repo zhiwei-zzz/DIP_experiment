abc5aa605a1506d821353e57322fe3a2795c485a
zzwalala <973286999@qq.com>
2021-01-17 16:16:59 +0800

    add comments

--- c++/main.cpp
+++ c++/main.cpp
@@ -7,14 +7,14 @@ Mat Mydilate(Mat src);
 Mat Myerode(Mat src);
 enum adaptiveMethod { meanFilter, gaaussianFilter, medianFilter };
 
-void MyAdaptiveThreshold(Mat& src, Mat& dst, double Maxval, int Subsize, double c, adaptiveMethod method = meanFilter) {
-
+void MyAdaptiveThreshold(Mat& src, Mat& dst, double Maxval, int Subsize, double c, adaptiveMethod method = meanFilter) //自适应阈值分割
+{
     Mat smooth = Mat::zeros(Size(src.cols, src.rows), CV_8UC1);
     int sub = (Subsize - 1) / 2;
     int sum, up, down, left, right;
     int cols = smooth.cols;
     int rows = smooth.rows;
-    for (int i = 0; i < cols; i++) {
+    for (int i = 0; i < cols; i++) {     //均值滤波
         for (int j = 0; j < rows; j++) {
             if (i < sub) left = i;
             else left = sub;
@@ -32,19 +32,18 @@ void MyAdaptiveThreshold(Mat& src, Mat& dst, double Maxval, int Subsize, double 
                 top = src.ptr<uchar>(j - up + k);
                 for (int l = 0; l < left + right + 1; l++) {
                     sum += top[i - left + l];
-                }              
+                }
             }
 
             uchar* target = smooth.ptr<uchar>(j);
             target[i] = int(sum / ((left + right + 1) * (up + down + 1)));
         }
     }
-
     //imshow("res", smooth);
 
-    smooth = smooth - c;
+    smooth = smooth - c; //全局减去参数值
     src.copyTo(dst);
-    for (int r = 0; r < src.rows; ++r) {
+    for (int r = 0; r < src.rows; ++r) {  //最后根据阈值判断为0还是255
         const uchar* srcptr = src.ptr<uchar>(r);
         const uchar* smoothptr = smooth.ptr<uchar>(r);
         uchar* dstptr = dst.ptr<uchar>(r);
@@ -60,21 +59,19 @@ void MyAdaptiveThreshold(Mat& src, Mat& dst, double Maxval, int Subsize, double 
 
 int main()
 {
-    int canny_threshold1, canny_threshold2;
+    int canny_threshold1, canny_threshold2;  //canny双参数阈值设定
     canny_threshold1 = 60;
-    canny_threshold2 = 110;
+    canny_threshold2 = 120;
 
-    Mat srcImg = imread("C:\\Users\\DELL\\Desktop\\DIP exp\\c++\\temp.bmp");
+    Mat srcImg = imread("C:\\Users\\DELL\\Desktop\\DIP exp\\c++\\temp.bmp");  //加载图片
     if (srcImg.empty()) {
         cout << "could not load image..." << endl;
         return -1;
     }
-
     /*imshow("Test opencv setup", srcImg);*/
 
     Mat Gray = Mat::zeros(Size(srcImg.cols, srcImg.rows), CV_8UC1);
-    for (int i = 0; i < srcImg.rows; i++) {
-        
+    for (int i = 0; i < srcImg.rows; i++) {  //转为灰度图  
         uchar* gray_data = Gray.ptr<uchar>(i);
         for (int j = 0; j < srcImg.cols; j++) {
             uchar* src_data = srcImg.ptr<uchar>(i, j);
@@ -84,13 +81,13 @@ int main()
     }
     Mat GrayThre = Mat::zeros(Size(srcImg.cols, srcImg.rows), CV_8UC1);
 
-    MyAdaptiveThreshold(Gray, GrayThre, 255, 77, 8, meanFilter);
+    MyAdaptiveThreshold(Gray, GrayThre, 255, 77, 8, meanFilter);   //自适应阈值分割用于检测褶皱
     //imshow("sdd", GrayThre);
 
 
     Mat Lap = Mat::zeros(Size(srcImg.cols, srcImg.rows), CV_8UC1);
 
-    for (int i = 1; i < Gray.rows - 1; i++) {
+    for (int i = 1; i < Gray.rows - 1; i++) {  //使用类拉普拉斯算子个权重为[[0, 1.5, 0], [1.5, -6, 1.5], [0, 1.5, 0]]
         uchar* data_up = Gray.ptr<uchar>(i - 1);
         uchar* data = Gray.ptr<uchar>(i);
         uchar* data_down = Gray.ptr<uchar>(i + 1);
@@ -103,22 +100,21 @@ int main()
     }
 
     Mat edge;
-    Canny(Lap, edge, canny_threshold1, canny_threshold2);
-    imshow("canny", edge);
+    MyCanny(Lap, edge, canny_threshold1, canny_threshold2); //调用Mycanny函数进行canny处理提取边缘
+    //imshow("canny", edge);
 
-    Mat dilate_kernel = Mat::ones(5, 5, CV_8UC1);
-    Mat dil_er0_t = Mat::zeros(Size(srcImg.cols, srcImg.rows), CV_8UC1);  
+    Mat dil_er0_t = Mat::zeros(Size(srcImg.cols, srcImg.rows), CV_8UC1);
     Mat dil_ero = Mat::zeros(Size(srcImg.cols, srcImg.rows), CV_8UC1);
-    dil_ero = Mydilate(edge);
+    dil_ero = Mydilate(edge); //进行膨胀
     dil_er0_t = Mydilate(GrayThre);
-    dil_ero = Myerode(dil_ero);
+    dil_ero = Myerode(dil_ero);  //进行腐蚀
     dil_er0_t = Myerode(dil_er0_t);
 
-    FoldDetect(dil_er0_t, srcImg);
+    FoldDetect(dil_er0_t, srcImg);   //检测折痕
 
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
-    findContours(dil_ero, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    findContours(dil_ero, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);  //检测边框
 
     int N = 20;
     int** l = new int* [3];//动态申请二维数组l 3 * N
@@ -155,11 +151,11 @@ int main()
     int t, g;
     t = g = 0;
     int i, j, lmax, lmin, hmin, hmax;
-    for (i = 0; i < contours.size(); i++) {
+    for (i = 0; i < contours.size(); i++) {         //对检测到的所有轮廓进行遍历
         lmax = hmax = 0;
         lmin = dil_ero.cols;
         hmin = dil_ero.rows;
-        for (j = 0; j < contours[i].size(); j++) {
+        for (j = 0; j < contours[i].size(); j++) {      //获取当前轮廓的最小外界矩形
             if (contours[i][j].x > lmax) lmax = contours[i][j].x;
             if (contours[i][j].x < lmin) lmin = contours[i][j].x;
             if (contours[i][j].y > hmax) hmax = contours[i][j].y;
@@ -167,52 +163,53 @@ int main()
         }
 
         int flag = 0;
-        uchar* data = Gray.ptr<uchar>(int((hmax + hmin) / 2));
-        if ((data[int((lmin + lmax) / 2)] < 127) && ((lmax - lmin) * (hmax - hmin) > 30)) {
+        uchar* data = Gray.ptr<uchar>(int((hmax + hmin) / 2));      //获取最小外接矩形的几何中心点灰度值
+        if ((data[int((lmin + lmax) / 2)] < 127) && ((lmax - lmin) * (hmax - hmin) > 30)) {     //虫子判别条件1：几何中心灰度值>127，且外接矩形面积<30
             int sum = 0;
             for (int i0 = 0; i0 < (hmax - hmin); i0++) {
                 for (int j0 = 0; j0 < (lmax - lmin); j0++) {
                     data = Gray.ptr<uchar>((hmin + i0));
-                    if (data[lmin + j0] < 100) sum += 1;
+                    if (data[lmin + j0] < 100) sum += 1;    //计算深色区域面积（阈值可调，实验条件下取值100较为理想）
                 }
             }
-            if (sum > int((hmax - hmin) * (lmax - lmin) / 2)) {
+            if (sum > int((hmax - hmin) * (lmax - lmin) / 2)) {     //虫子判别条件2：外接矩形深色区域面积大于送面积的一半（可以认为虫子，杂物一般为矩形或圆形）
                 m[0][g] = lmin;
                 m[1][g] = lmax;
                 m[2][g] = hmin;
                 m[3][g] = hmax;
                 m[4][g] = contourArea(contours[i]);
                 g += 1;
-                drawContours(srcImg, contours, i, Scalar(255, 140, 0), 3);
-                flag = 1;
+                drawContours(srcImg, contours, i, Scalar(255, 140, 0), 3);  //确定其外围矩形的位置并勾勒轮廓、计算面积
+                flag = 1;                       //符合条件1、2则判定为虫子，计标记为1
             }
         }
-        if (flag != 1)
+        if (flag != 1)      //若判断不是虫子，则判断是否为头发
         {
-            if (((lmax - lmin) * (hmax - hmin)) > 1000) {
+            if ((((lmax - lmin) * (hmax - hmin)) > 1000) && (contourArea(contours[i]) / arcLength(contours[i], true) < 0.03)) {
+                //头发判别条件1：头发一般较长且尺寸较大，设定外接矩形面积阈值为1000；头发判别条件2：周长面积比很小，设置阈值0.03
                 l[0][t] = lmax;
                 l[1][t] = lmin;
                 h[0][t] = hmax;
                 h[1][t] = hmin;
                 l[2][t] = arcLength(contours[i], true) / 2;
-                drawContours(srcImg, contours, i, Scalar(15, 25, 105), 2);
+                drawContours(srcImg, contours, i, Scalar(15, 25, 105), 2);//确定外围矩形的位置并计算长度，头发丝极细，可以认为发丝长度为周长的一半
                 t += 1;
             }
         }
 
     }
-    for (int k = 0; k < t; k++) {
+    for (int k = 0; k < t; k++) {  //框出头发并显示长度
         if (l[0][k] != 0 && l[1][k] != srcImg.cols) {
             rectangle(srcImg, Rect(l[1][k] - 5, h[1][k] - 5, l[0][k] + 5 - (l[1][k] - 5), h[0][k] + 5 - (h[1][k] - 5)), (255, 0, 0), 3);
             putText(srcImg, "H:" + to_string(l[2][k]), Point(l[0][k] + 5, h[0][k] + 5), FONT_HERSHEY_COMPLEX, 1, (0, 50, 255), 2);
 
         }
     }
-    for (int k = 0; k < g; k++) {
+    for (int k = 0; k < g; k++) {   //框出异物并显示面积
         rectangle(srcImg, Rect(m[0][k] - 3, m[2][k] - 3, m[1][k] + 3 - (m[0][k] - 3), m[3][k] + 3 - (m[2][k] - 3)), (255, 0, 0), 3);
         putText(srcImg, "'B:" + to_string(m[4][k]), Point(m[1][k] - 10, m[3][k] - 15), FONT_HERSHEY_COMPLEX, 1, (0, 50, 255), 2);
     }
-    cout << "There are " << to_string(t + g) << "defects being detected!" << endl;
+    cout << "There are " << to_string(t + g) << "defects being detected!" << endl;  //输出文字结果
 
     imshow("check", srcImg);
 
@@ -223,45 +220,45 @@ int main()
 }
 
 
-void FoldDetect(cv::Mat& mat, cv::Mat& image)
+void FoldDetect(cv::Mat& mat, cv::Mat& image)  //消去相近的直线
 {
     int minLineLength, maxLineGap;
     minLineLength = 90;
     maxLineGap = 100;
     vector<Vec4i> Lines;
-    HoughLinesP(mat, Lines, 1, 3.1415926 / 180, 60, minLineLength, maxLineGap);
+    HoughLinesP(mat, Lines, 1, 3.1415926 / 180, 60, minLineLength, maxLineGap);  //霍夫直线提取
     Mat Mask = Mat::zeros(Size(mat.cols, mat.rows), CV_8UC1);
     int a;
     a = 0;
 
     int t1, t2;
     static vector <Vec4i> sum;
-    for (rsize_t i = 0; i < Lines.size(); i++) {
+    for (rsize_t i = 0; i < Lines.size(); i++) {  //对每一条直线进行分析
         static int begin = 1;
-        Vec4i I = Lines[i];
+        Vec4i I = Lines[i]; //取第一条直线
 
 
-        t1 = max(I[0], I[2]) + 30;
+        t1 = max(I[0], I[2]) + 30;   //考虑一能框住完整直线的矩形， 判断其中有多少条直线
         t2 = min(I[0], I[2]) - 30;
 
-        if ((min(I[0], I[2]) > t2) && (max(I[0], I[2]) < t1)) {
+        if ((min(I[0], I[2]) > t2) && (max(I[0], I[2]) < t1)) { //若在矩形内，计数加1
             a += 1;
         }
-        if (a > 10) {
-            if (begin == 1) {
+        if (a > 10) { //若有超过十条直线，将线段入队列
+            if (begin == 1) {  //推入队列前队列中无其他直线
                 sum.push_back(I);
 
                 begin = 0;
             }
-            if (begin != 1)
+            if (begin != 1)  //此时队列中已有直线，需将Lines中直线与队列中进行比较，仅有当该直线与队列中直线具有一定距离时，直线入栈
             {
                 rsize_t j = 0;
                 for (j = 0; j < sum.size(); j++) {
 
                     Vec4i member = sum[j];
-                    float distance1 = sqrt(pow((I[0] - member[0]), 2) + pow((I[1] - member[1]), 2));
+                    float distance1 = sqrt(pow((I[0] - member[0]), 2) + pow((I[1] - member[1]), 2));  //进行平方和运算计算距离
                     float distance2 = sqrt(pow((I[0] - member[2]), 2) + pow((I[1] - member[3]), 2));
-                    if ((distance1 < 200) || (distance2 < 200)) break;
+                    if ((distance1 < 200) || (distance2 < 200)) break; //若有一端端点间距离小于200， 即退出。
 
                 }
                 if (j == (sum.size())) {
@@ -275,22 +272,19 @@ void FoldDetect(cv::Mat& mat, cv::Mat& image)
     }
     for (rsize_t i = 0; i < sum.size(); i++) {
         Vec4i I = sum[i];
-        line(image, Point(I[0], I[1]), Point(I[2], I[3]), (30, 150, 0), 5);
+        line(image, Point(I[0], I[1]), Point(I[2], I[3]), (30, 150, 0), 5);//绘画队列中直线
     }
 }
 
-Mat Mydilate(Mat src)
+Mat Mydilate(Mat src)  //膨胀函数 为5*5的ones核，中心为原点
 {
     Mat dilated_my;
     dilated_my.create(src.rows, src.cols, CV_8UC1);
-    for (int i = 0; i < src.rows; ++i)
+    for (int i = 0; i < src.rows; ++i)  //遍历所有
     {
         for (int j = 0; j < src.cols; ++j)
         {
-            //uchar minV = 255;
             uchar maxV = 0;
-
-            //遍历周围最大像素值
             for (int yi = i - 2; yi <= i + 2; yi++)
             {
                 for (int xi = j - 2; xi <= j + 2; xi++)
@@ -299,8 +293,7 @@ Mat Mydilate(Mat src)
                     {
                         continue;
                     }
-                    //minV = (std::min<uchar>)(minV, img.at<uchar>(yi, xi));
-                    maxV = (std::max<uchar>)(maxV, src.at<uchar>(yi, xi));
+                    maxV = (std::max<uchar>)(maxV, src.at<uchar>(yi, xi)); //取最大值
                 }
             }
             dilated_my.at<uchar>(i, j) = maxV;
@@ -309,17 +302,15 @@ Mat Mydilate(Mat src)
     return dilated_my;
 }
 
-Mat Myerode(Mat src)
+Mat Myerode(Mat src)  //腐蚀函数 为5*5的ones核，中心为原点
 {
     Mat dilated_my;
     dilated_my.create(src.rows, src.cols, CV_8UC1);
-    for (int i = 0; i < src.rows; ++i)
+    for (int i = 0; i < src.rows; ++i) //遍历所有
     {
         for (int j = 0; j < src.cols; ++j)
         {
             uchar minV = 255;
-
-            //遍历周围最大像素值
             for (int yi = i - 2; yi <= i + 2; yi++)
             {
                 for (int xi = j - 2; xi <= j + 2; xi++)
@@ -328,8 +319,8 @@ Mat Myerode(Mat src)
                     {
                         continue;
                     }
-                    //minV = (std::min<uchar>)(minV, img.at<uchar>(yi, xi));
-                    minV = (std::min<uchar>)(minV, src.at<uchar>(yi, xi));
+
+                    minV = (std::min<uchar>)(minV, src.at<uchar>(yi, xi));  //取最小值
                 }
             }
             dilated_my.at<uchar>(i, j) = minV;
